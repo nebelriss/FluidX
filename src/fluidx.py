75969ec6a2e50331e0de409dab390054d751b6ce
Michel <nebelriss@gmail.com>
2010-12-08 18:20:08 +0100

    init fluidx main file

---
+++ src/fluidx.py
@@ -0,0 +1,17 @@
+'''
+Created on 8 Dec 2010
+
+@author: nebelriss
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
