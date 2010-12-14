cd1e310ce711fa5146f5db95efbfb76344f0c39c
Michel <nebelriss@gmail.com>
2010-12-14 22:07:30 +0100

    changes

--- src/plot.py
+++ src/plot.py
@@ -20,6 +20,6 @@ class Plot(object):
         self.canvas = Canvas(frame, bg = "white")
         self.canvas.pack(expand = YES, fill = BOTH)
         
-        self.canvas.create_line(self.sw-315, self.sh-150, 50, self.sh-150, fill = "black", width = 3)
-        self.canvas.create_line(70, self.sh-130, 70, 70, fill = "black", width = 3)
-        
+        self.canvas.create_line(self.sw-315, self.sh-150, 50, self.sh-150, fill = "black", width = 1)
+        self.canvas.create_line(70, self.sh-130, 70, 70, fill = "black", width = 1)
+                
