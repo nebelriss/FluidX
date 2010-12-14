13503653625ce5313cf856a8d21960d2b2afa9fb
Michel <nebelriss@gmail.com>
2010-12-14 21:38:35 +0100

    checkbox init

---
+++ src/checkbox.py
@@ -0,0 +1,29 @@
+'''
+Created on 14 Dec 2010
+
+@author: nebelriss
+'''
+
+from Tkinter import *
+
+class Checkbox(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, frame):
+        '''
+        Constructor
+        '''
+        #Labels for Listbox
+        motor_label = Label(frame, text = "motors")
+        motor_label.pack(side = TOP, pady = 20, padx = 20, anchor= W)
+        motor_listbox = Listbox(frame)
+        motor_listbox.pack(side=TOP, padx = 20)
+        
+        # Listbox
+        medium_label = Label(frame, text = "mediums")
+        medium_label.pack(side = TOP, pady = 20, padx = 20, anchor= W)
+        medium_listbox = Listbox(frame)
+        medium_listbox.pack(side=TOP, padx = 20)
