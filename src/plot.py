322bce08add62ad9d960a1a3da543b18c25f9425
Sandra <Sandra@LangSandra-PC.edu.ds.fhnw.ch>
2010-12-15 15:59:28 +0100

    plot

--- src/plot.py
+++ src/plot.py
@@ -21,4 +21,20 @@ class Plot(object):
         
         self.canvas.create_line(self.sw-315, self.sh-150, 50, self.sh-150, fill = "black", width = 1)
         self.canvas.create_line(70, self.sh-130, 70, 70, fill = "black", width = 1)
-                
+        
+        #Kennzeichnung der Achsenlinien
+        y = 'Temperatur'
+        x = 'Zeit'
+        self.canvas.create_text(70,40, text= y)
+        self.canvas.create_text(self.sw-315,self.sh-130, text= x)
+        
+        #Beschriftung der Achsenlinien
+        for i in range(12):
+            x = 70 + (i*40)
+            self.canvas.create_line(x,500,x,495, width = 2)
+            self.canvas.create_text(x,510, text='%d'% (20*i), anchor=N)   
+        for i in range (12):
+            y = 71 + (i*39)   
+            self.canvas.create_line(70,y,75,y, width = 2)
+            self.canvas.create_text(45,y, text='%d'% (-i+11), anchor=W)
+                    
