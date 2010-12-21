bd4bc935fa038de8fde5c6a5873f8b5db8e6dd56
Michel <nebelriss@gmail.com>
2010-12-21 09:58:39 +0100

    added listbox

--- src/main_ui.py
+++ src/main_ui.py
@@ -8,6 +8,7 @@ from data_access import *
 from plot import *
 from table import *
 from data import *
+from Listboxes import *
 
 
 class Main():
@@ -25,8 +26,8 @@ class Main():
         self.data = Data()
         self.values_height = 3
         self.values_id = 0
-        self.motor_control = []
-        self.media_control = []
+        self.idx = 0
+        self.i = 0
 
         
         # Frames
@@ -57,42 +58,6 @@ class Main():
         button_table.pack(side = LEFT, pady = 2)
         button_exit.pack(side = RIGHT, pady = 2, padx = 5)
         
-        #right panel#
-        #Labels for Listbox
-        motor_label = Label(self.right_frame, text = "motors")
-        motor_label.pack(side = TOP, pady = 20, padx = 20, anchor= W)
-        self.motor_listbox = Listbox(self.right_frame, selectmode = SINGLE, height = 4)
-        self.motor_listbox.pack(side=TOP, padx = 20)      
-       
-        # read selected in motor_listbox
-        self.motor_listbox.bind("<<ListboxSelect>>", self.sel_motor)
-            
-        
-        # Listbox
-        medium_label = Label(self.right_frame, text = "mediums")
-        medium_label.pack(side = TOP, pady = 20, padx = 20, anchor= W)
-        self.medium_listbox = Listbox(self.right_frame, selectmode = SINGLE, height = 3)
-        self.medium_listbox.pack(side=TOP, padx = 20)
-        
-        #read selected in medium_listbox
-        self.medium_listbox.bind("<<ListboxSelect>>", self.sel_medium)
-        
-        
-        
-        
-        # Values label
-        ex_label = Label(self.right_frame, text = "Experiments")
-        ex_label.pack(side = TOP, anchor = W, pady = 20, padx = 20)
-    
-        
-        # Frame for checkboxes
-        self.values_frame = Frame(self.right_frame)
-        self.values_frame.pack(side =TOP, padx = 20, fill = BOTH, expand = NO)
-        self.values_listbox = Listbox(self.values_frame, selectmode = MULTIPLE, height = 4)
-        self.values_listbox.pack(side = TOP, fill = BOTH, expand = YES)
-        self.values_listbox.bind("<<ListboxSelect>>", self.sel_values)
-        
-        
         self.plot()
         
         
@@ -114,11 +79,20 @@ class Main():
         '''
         
         ''' 
-        plot = Plot(root, self.plot_frame, self.sw, self.sh)
+        self.plot = Plot(root, self.plot_frame, self.sw, self.sh)
 ###################################################################################################
 
+#updateplot##############################################################################################      
+    def updateplot(self, meta, values):
+        '''
         
-      
+        ''' 
+        canvas_data = []
+        canvas_data.append([])
+        print canvas_data
+###################################################################################################
+
+
 #save data##########################################################################################           
     def table(self):
         '''
@@ -145,92 +119,20 @@ class Main():
         '''
 
         '''
-        # store datas to lists
-        self.values, self.meta = self.data.getdata()
-        self.canvas_data = []
-        # refresh entries in the listboxes
-        for i in range(0, len(self.meta)):
-            meta_tmp = self.meta[i]
-            motor_meta = meta_tmp['exp_name']
-            # control if entry already exists
-            if len(self.motor_control) == 0:
-                self.motor_control.append(motor_meta)
-                # insert into the_listboxes
-                self.motor_listbox.insert(END, "M: " + str(motor_meta))
-            else:
-                for m in range(0,len(self.motor_control)):
-                    if motor_meta != self.motor_control[m]:
-                        self.motor_control.append(motor_meta)
-                        # insert into the_listboxes
-                        self.motor_listbox.insert(END, "M: " + str(motor_meta))
-                    else:
-                        pass
-                    
-
-        for i in range(0, len(self.meta)):
-            meta_tmp = self.meta[i]
-            media_meta = meta_tmp['additional_info']
-            # control if entry already exists
-            if len(self.media_control) == 0:
-                self.media_control.append(media_meta)
-                # insert into the_listboxes
-                self.medium_listbox.insert(END, str(media_meta))
-            else:
-                for m in range(0,len(self.media_control)):
-                    print "shit"
-                    if media_meta != self.media_control[m]:
-                        print "shit2"
-                        self.media_control.append(media_meta)
-                        # insert into the_listboxes
-                        self.medium_listbox.insert(END, str(media_meta))
-                    else:
-                        print "shit3"
-                        pass
-            
-            # create checkboxes
-
-            self.canvas_data.append([])
-            for j in range(0, len(self.values)):
-                values = self.values[j]
-                for row in values:
-                    self.values_id += 1
-                    box_name = str(self.values_id) + ":  " + str(row[2]) + "/" + str(row[1]) + "/" + str(row[3])
-                    self.values_listbox.insert(END, str(box_name))
-                    self.values_listbox.configure(height = self.values_height)
-                    self.values_height += 1
-                    # wirte infos to List
-                    self.canvas_data[j].append([meta_tmp, row])
-
+        values, meta = self.data.getdata()
+        self.create_Listbox(meta, values, self.idx)
+        self.idx += 1
 ###################################################################################################
 
 
 
-# events for values_listbox #############################################################################       
-    def sel_values(self, event):
-        '''
-        
-        '''
-
-        for i in range(len(self.values_listbox.curselection())):
-            self.selected_values = self.values_listbox.get(self.values_listbox.curselection()[i])
-            sel_nr = self.selected_values[0]
-            for row in self.canvas_data:
-                meta = row[int(sel_nr)][0]
-                values = row[int(sel_nr)][1]
-                
-                plot.createCanvas(meta, row)
-
-        print
-################################################################################################## 
-
-
-       
 #importer##########################################################################################
     def importer(self):
         '''
         opens the importer window
         '''
-        self.i = Import_ui(root)
+        self.importer = Import_ui(root)
+        print str(self.importer)
 ##################################################################################################         
         
 
@@ -250,31 +152,75 @@ class Main():
         databasepath = self.databasename.get()
         self.savedata(databasepath)
 ################################################################################################## 
-        
 
-
-        
-# events for motor and medium listbox #############################################################################       
-    def sel_motor(self, event):
+    def create_Listbox(self, meta, values, idx):
         '''
         
         '''
-        for i in range(len(self.motor_listbox.curselection())):
-            self.selected_motor = self.motor_listbox.get(self.motor_listbox.curselection()[i])
-            print self.selected_motor
+        values_height = 1
+        values_frame = Frame(self.right_frame)
+        values_frame.pack(side =TOP, padx = 20, fill = BOTH, expand = NO, pady = 10)
+        
+        # get meta
+        meta_tmp = meta[idx]
+        date_meta = str(meta_tmp['date'])
+        motor_meta = str(meta_tmp['exp_name'])
+        media_meta = str(meta_tmp['additional_info'])
+        self.i += 1
+        
+        #convert date
+        date_txt = date_meta[6:8] + "." + date_meta[4:6] + "." + date_meta[:4] + " " + date_meta[8:10] + ":" + date_meta[10:]
+        motor_txt = "Motor: " + motor_meta
+        media_txt = "Media: " + media_meta
+        # create Labels
+        motor_label = Label(values_frame, text = motor_txt)
+        motor_label.pack(side = TOP, anchor = W)
+        
+        media_label = Label(values_frame, text = media_txt)
+        media_label.pack(side = TOP, anchor = W)
+        
+        date_label = Label(values_frame, text = date_txt)
+        date_label.pack(side = TOP, anchor = W)
+        
+        # create Listbox
 
-    def sel_medium(self, event): 
-        '''
-         
-        '''
-        self.selected_medium = self.medium_listbox.get(self.medium_listbox.curselection()[0])
-        print self.selected_medium
-##################################################################################################        
+        self.values_listbox = Listbox(values_frame, selectmode = MULTIPLE, height = values_height, exportselection=0)
+        self.values_listbox.pack(side = TOP, fill = BOTH, expand = YES)
+        self.values_listbox.bind("<<ListboxSelect>>", self.sel_values)
+        
+        self.canvas_data = []
+        values_id = 0
+        values = values[idx]
+        print self.idx
+        for row in values:
+            values_id += 1
+            box_name = str(values_id) + ":  " + str(row[2]) + "/" + str(row[1]) + "/" + str(row[3])
+            self.values_listbox.insert(END, str(box_name))
+            self.values_listbox.configure(height = values_height)
+            values_height += 1
+            # wirte infos to List
+            self.canvas_data.append([meta_tmp, row])
+
+            
+                
+    def sel_values(self, event):
+        sel_values_list = []
+        for i in range(len(self.values_listbox.curselection())):
+            sel_values = self.values_listbox.get(self.values_listbox.curselection()[i])
+            sel_nr = sel_values[0]
+            row = self.canvas_data[int(sel_nr)]
+            meta_plot = row[0]
+            values = row[1]
+            print values
+            sel_values_list.append(values)
+            
+        # update plot with the new datas    
+        self.updateplot(meta_plot, sel_values_list)
+        
         
 
 
 root = Tk()
 root.title("FluidX - 0.1")
-
 main = Main(root)
 root.mainloop()
