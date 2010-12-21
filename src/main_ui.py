ef8fd03686187307658d4b870ac2e7967298215d
Michel <nebelriss@gmail.com>
2010-12-21 11:03:25 +0100

    cleared code

--- src/main_ui.py
+++ src/main_ui.py
@@ -9,6 +9,7 @@ from plot import *
 from table import *
 from data import *
 from Listboxes import *
+import threading
 
 
 class Main():
@@ -120,7 +121,9 @@ class Main():
 
         '''
         values, meta = self.data.getdata()
-        self.create_Listbox(meta, values, self.idx)
+        lister = Listboxes(self.right_frame, meta, values, self.idx, self.plot)
+#        lister.start()
+        #lister.create_Listbox(meta, values, self.idx)
         self.idx += 1
 ###################################################################################################
 
@@ -153,69 +156,6 @@ class Main():
         self.savedata(databasepath)
 ################################################################################################## 
 
-    def create_Listbox(self, meta, values, idx):
-        '''
-        
-        '''
-        values_height = 1
-        values_frame = Frame(self.right_frame)
-        values_frame.pack(side =TOP, padx = 20, fill = BOTH, expand = NO, pady = 10)
-        
-        # get meta
-        meta_tmp = meta[idx]
-        date_meta = str(meta_tmp['date'])
-        motor_meta = str(meta_tmp['exp_name'])
-        media_meta = str(meta_tmp['additional_info'])
-        self.i += 1
-        
-        #convert date
-        date_txt = date_meta[6:8] + "." + date_meta[4:6] + "." + date_meta[:4] + " " + date_meta[8:10] + ":" + date_meta[10:]
-        motor_txt = "Motor: " + motor_meta
-        media_txt = "Media: " + media_meta
-        # create Labels
-        motor_label = Label(values_frame, text = motor_txt)
-        motor_label.pack(side = TOP, anchor = W)
-        
-        media_label = Label(values_frame, text = media_txt)
-        media_label.pack(side = TOP, anchor = W)
-        
-        date_label = Label(values_frame, text = date_txt)
-        date_label.pack(side = TOP, anchor = W)
-        
-        # create Listbox
-
-        self.values_listbox = Listbox(values_frame, selectmode = MULTIPLE, height = values_height, exportselection=0)
-        self.values_listbox.pack(side = TOP, fill = BOTH, expand = YES)
-        self.values_listbox.bind("<<ListboxSelect>>", self.sel_values)
-        
-        self.canvas_data = []
-        values_id = 0
-        values = values[idx]
-        print self.idx
-        for row in values:
-            values_id += 1
-            box_name = str(values_id) + ":  " + str(row[2]) + "/" + str(row[1]) + "/" + str(row[3])
-            self.values_listbox.insert(END, str(box_name))
-            self.values_listbox.configure(height = values_height)
-            values_height += 1
-            # wirte infos to List
-            self.canvas_data.append([meta_tmp, row])
-
-            
-                
-    def sel_values(self, event):
-        sel_values_list = []
-        for i in range(len(self.values_listbox.curselection())):
-            sel_values = self.values_listbox.get(self.values_listbox.curselection()[i])
-            sel_nr = sel_values[0]
-            row = self.canvas_data[int(sel_nr)]
-            meta_plot = row[0]
-            values = row[1]
-            print values
-            sel_values_list.append(values)
-            
-        # update plot with the new datas    
-        self.updateplot(meta_plot, sel_values_list)
         
         
 
