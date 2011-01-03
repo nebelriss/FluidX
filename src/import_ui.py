84221fa0611576ccaacd16aaac1bedbba7e2dbb4
Michel <nebelriss@gmail.com>
2011-01-03 14:09:45 +0100

    sepearted label text

--- src/import_ui.py
+++ src/import_ui.py
@@ -11,10 +11,13 @@ from data import *
 
 
 
-class Import_ui(object):
+class Import(object):
     '''
     This Class is for importing CSV files with a specific format that is given out to the creators.
-    First the file will be read with csvreader and stored in a sqlite3 database. 
+    First the file will be read with csvreader and stored in a sqlite3 database.
+    
+    The latest documentation about the csv file-format can be found at:
+        https://github.com/nebelriss/FluidX/csv-file.pdf
     '''
 
     def __init__(self, root):
@@ -28,37 +31,58 @@ class Import_ui(object):
         self.child.title("Import - FluidX")
         self.child.resizable(width=FALSE, height=FALSE)
         
-        # init
+        # inits
         self.value = IntVar()
         self.filepath = StringVar()
         self.databasename = StringVar()
         
+        # Label values
+        csv_title = "CSV - Import"
+        dir_txt = "Folder: "
+        given_path = "~/file.csv"
+        newdb_txt = "Create new Database"
+        exdb_txt = "Create new Database"
+        browse_txt = "Browse"
+        ok_txt = "OK"
+        cancel_txt  = "Cancel"
+        
         
         # main Frame
         self.frame = Frame(self.child)
-        # grid frame
-        self.frame.grid() 
-        # Title showed inside the window  
-        self.csvLabel = Label(self.frame, text = "CSV - Import")
+        self.frame.grid()         
+        
+
+        # Title showed inside the window
+        csvLabel = Label(self.frame, text = csv_title)
+        self.csvLabel.grid(row = 0, column = 0, columnspan = 2, sticky = (N, W), pady = 10, padx = 5)
+        
+        # changing font and raising size of the title       
         myfont = Font(family = 'Arial', size = 14, weight = BOLD)
-        self.csvLabel.config(font = myfont)
-        self.dirLabel = Label(self.frame, text = "Folder: ")
+        csvLabel.config(font = myfont)
+
+        
+        # Directory and emtpylabel for a space
+        self.dirLabel = Label(self.frame, text = dir_txt)
+        self.dirLabel.grid(row = 1, column = 0, padx = 5)
+             
         self.emptylabel = Label(self.frame, height = 1)
+        self.emptylabel.grid(row = 2, column = 0)
+       
+        
         #Entry
         self.entryPath = Entry(self.frame, width = 40, textvariable = self.filepath, bg = "grey")
-        self.entryPath.insert(END, "~/file.csv")
+        self.entryPath.insert(END, given_path)
         # Radiobuttons, new Database or existing database
-        self.newDB = Radiobutton(self.frame, text = "Create new Database", variable = self.value, value = 0)
-        self.existDB = Radiobutton(self.frame, text = "Use existing Database", variable = self.value, value = 1)
+        self.newDB = Radiobutton(self.frame, text = newdb_txt, variable = self.value, value = 0)
+        self.existDB = Radiobutton(self.frame, text = exdb_txt, variable = self.value, value = 1)
         # Buttons to start import and quit the import window
-        self.buttonBrowse = Button(self.frame, text = "Browse", command = self.csvopenfilename)
-        self.buttonOK = Button(self.frame, text = "OK", width = 5, command = self.cvstosql)
-        self.buttonCancel = Button(self.frame, text = "Cancel", command = self.child.destroy)
+        self.buttonBrowse = Button(self.frame, text = browse_txt, command = self.csvopenfilename)
+        self.buttonOK = Button(self.frame, text = ok_txt, width = 5, command = self.cvstosql)
+        self.buttonCancel = Button(self.frame, text = cancel_txt, command = self.child.destroy)
         
         # grit for widget, position etc.
-        self.csvLabel.grid(row = 0, column = 0, columnspan = 2, sticky = (N, W), pady = 10, padx = 5)
-        self.dirLabel.grid(row = 1, column = 0, padx = 5)
-        self.emptylabel.grid(row = 2, column = 0)
+
+
         self.entryPath.grid(row = 1, column = 1, columnspan = 1, sticky = (N, W), padx = 5, pady = 2.5)
         self.buttonBrowse.grid(row = 1, column = 2, sticky = W, padx = 5)
         self.newDB.grid(row = 3, column = 1, sticky = W, columnspan = 2)
@@ -85,7 +109,7 @@ class Import_ui(object):
         only writes the path to self.databasename
         '''
         self.file_save = options = {}
-        options['filetypes'] = [('tes files', '.tes')]
+        options['filetypes'] = [('sqlite3 files', '.sqlite3')]
         options['initialdir'] = 'C:\\'
         options['parent'] = self.child
         options['title'] = 'Save your Database file'
@@ -97,7 +121,7 @@ class Import_ui(object):
         opens a dialoge where the user can chose the location of an existing database, the path is saved then in databasename.
         '''
         self.file_open = options = {}
-        options['filetypes'] = [('tes files', '.tes')]
+        options['filetypes'] = [('sqlite3 files', '.sqlite3')]
         options['initialdir'] = 'C:\\'
         options['parent'] = self.child
         options['title'] = 'Choose your existing database file'
