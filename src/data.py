9c1fe871d682611ad728d4bf02fd8e2c40ec6755
Michel <nebelriss@gmail.com>
2011-01-04 10:24:07 +0100

    changes

--- src/data.py
+++ src/data.py
@@ -1,28 +1,52 @@
 #!/usr/bin/python
 # encoding: utf-8
+
 from data_access import *
 
 
 
 class Data():
+    '''
+    Initial version by:
+        Michel Heiniger and Sandra Lang
+    
+    Latest source code can be found at:
+        https://github.com/nebelriss/FluidX
+    '''
     
     def __init__(self):
+        '''
+        constructor for empty lists
+        '''
+        # initialize empty list for values and meta
         self.values = []
         self.meta = []
  
     def savedata(self, dbpath):
+        '''
+        
+        '''
+        # open sqlite3 connection
         db = Experiment(dbpath)
+        
+        # write values and meta to sqlite3 file
         values = db.load_values()
         meta = db.load_metadata()
+        
+        # send values and meta to store them in a list
         self.storedata(values, meta)
-        print dbpath
 
 
     def storedata(self, v, m):
+        '''
+        storing meta and values in a list
+        '''
         self.values.append(v)
         self.meta.append(m)
 
     
     def getdata(self):
-       
+        '''
+        returns values and meta
+        '''       
         return self.values, self.meta
